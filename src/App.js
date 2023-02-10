ec3cd5a7aa973b06dba5a44c2975009fb3bf48c3
Camilo-J <josecamilo1902@outlook.com>
2023-02-09 22:46:30 -0500

    Refactor styles

--- src/App.js
+++ src/App.js
@@ -16,16 +16,30 @@ function App() {
       </h1>
 
       <div className="flex justify-center">
-        <table className="table-auto  w-10/12">
+        <table className="table-auto  w-10/12 border-separate border-spacing-y-4">
           <thead>
-            <tr className="border-b-2 border-teal-500">
-              <th className="p-4 text-teal-500 text-start">Nombre</th>
-              <th className="p-4 text-teal-500 text-start">Apellido</th>
-              <th className="p-4 text-teal-500 text-start">Edad</th>
-              <th className="p-4 text-teal-500 text-start">Genero</th>
-              <th className="p-4 text-teal-500">Email</th>
-              <th className="p-4 text-teal-500 text-start">Nacionalidad</th>
-              <th className="p-4 text-teal-500 text-start">Foto</th>
+            <tr>
+              <th className="p-4 text-teal-500 text-start border-y-2 border-l-2 border-teal-500 rounded-tl-md rounded-bl-md">
+                Nombre
+              </th>
+              <th className="p-4 text-teal-500 text-start border-y-2 border-teal-500">
+                Apellido
+              </th>
+              <th className="p-4 text-teal-500 text-start border-y-2 border-teal-500">
+                Edad
+              </th>
+              <th className="p-4 text-teal-500 text-start border-y-2 border-teal-500">
+                Genero
+              </th>
+              <th className="p-4 text-teal-500 border-y-2 border-teal-500">
+                Email
+              </th>
+              <th className="p-4 text-teal-500 text-start border-y-2 border-teal-500">
+                Nacionalidad
+              </th>
+              <th className="p-4 text-teal-500 text-start border-y-2 border-teal-500 border-r-2 rounded-tr-md rounded-br-md">
+                Foto
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -35,17 +49,27 @@ function App() {
                 return (
                   <tr
                     key={index}
-                    className="border-b border-teal-500 hover:bg-teal-300/20 "
+                    className="transition-transform duration-500 hover:scale-105 "
                   >
-                    <td className="px-4 text-gray-700">{user.name.first}</td>
-                    <td className="px-4 text-gray-700">{user.name.last}</td>
-                    <td className="px-4 text-gray-700">{user.dob.age}</td>
-                    <td className="px-4 text-gray-700">{user.gender}</td>
-                    <td className="px-4 text-gray-700 text-center">
+                    <td className="px-4 text-gray-700 border-l border-y rounded-tl-md rounded-bl-md border-teal-500">
+                      {user.name.first}
+                    </td>
+                    <td className="px-4 text-gray-700 border-y border-teal-500">
+                      {user.name.last}
+                    </td>
+                    <td className="px-4 text-gray-700 border-y border-teal-500">
+                      {user.dob.age}
+                    </td>
+                    <td className="px-4 text-gray-700 border-y border-teal-500">
+                      {user.gender}
+                    </td>
+                    <td className="px-4 text-gray-700 text-center border-y border-teal-500">
                       {user.email}
                     </td>
-                    <td className="px-4 text-gray-700">{user.nat}</td>
-                    <td className="p-3">
+                    <td className="px-4 text-gray-700 border-y border-teal-500">
+                      {user.nat}
+                    </td>
+                    <td className="p-3 border-y border-r border-teal-500 rounded-tr-md rounded-br-md">
                       <img
                         className="rounded-full"
                         src={user.picture.thumbnail}
